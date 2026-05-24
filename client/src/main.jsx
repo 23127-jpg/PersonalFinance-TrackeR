cf136d6ed818b7adead26cd961dab11c4c1d81c2
Harshit <harshitk1923@gmail.com>
2026-05-24 17:44:39 +0530

    fix: remove axios from main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import axios from 'axios';
+
 
 import App from './App.jsx';
 import './index.css';
 
 // Configure global Axios baseURL for seamless local/production routing
-axios.defaults.baseURL = 'https://personalfinance-tracker.onrender.com';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
